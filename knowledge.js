90de41219830c210d13ae877997f73754852dd61
Anas Shaaban <120345114+yusefshaaban@users.noreply.github.com>
2025-09-27 00:53:51 +0100

    Completed knowledge.html. Includes all computer softwares, human languages, and a link to CodeCademy profile. When user hovers over a software or language, the name is displayed underneath.

---
+++ knowledge.js
@@ -0,0 +1,22 @@
+humanLanguages = document.getElementsByClassName('human-language');
+humanLanguageLabels = document.getElementsByClassName('human-language-label');
+for (let i = 0; i < humanLanguages.length; i++) {
+    humanLanguages[i].addEventListener('mouseover', function() {
+        humanLanguageLabels[i].style.display = "block";
+    });
+    humanLanguages[i].addEventListener('mouseout', function() {
+            humanLanguageLabels[i].style.display = "none";
+    });
+}
+
+computerSoftware = document.getElementsByClassName('computer-software');
+computerSoftwareLabels = document.getElementsByClassName('computer-software-label');
+for (let i = 0; i < computerSoftware.length; i++) {
+    computerSoftware[i].addEventListener('mouseover', function() {
+        computerSoftwareLabels[i].style.display = "block";
+    });
+    computerSoftware[i].addEventListener('mouseout', function() {
+        computerSoftwareLabels[i].style.display = "none";
+    });
+}
+
